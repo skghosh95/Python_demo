65ff99724e858adb648d16e1be899051279c2a93
Suman Kalyan Ghosh <134367203+skghosh95@users.noreply.github.com>
2023-09-19 11:18:45 +0530

    Update Python_Practice.py

--- Python_Practice.py
+++ Python_Practice.py
@@ -34,11 +34,13 @@ first_name = "Tony"
 last_name = "Stark"
 age = 52
 is_genius = True
-Taking Input
+
+#Taking Input
 name = input("What is your name? ")
 print("Hello " + name)
 print("Welcome to our cool Python class")
-> Exercise Solution
+
+#> Exercise Solution
 superhero = input("What is your superhero name? ")
 print(superhero)
 Type Conversion
@@ -47,17 +49,20 @@ old_age = input("Enter your age : ")
 #print(new_age)
 new_age = int(old_age) + 2
 print(new_age)
-#Useful converion functions
+
+#Useful conversion functions
 # 1. float()
 # 2. bool()
 # 3. str()
 # 4. int()
-> Code for Sum of 2 Numbers
+
+#> Code for Sum of 2 Numbers
 first_number = input("Enter 1st number : ")
 second_number = input("Enter 2nd number : ")
 sum = float(first_number) + float(second_number)
 print("the sum is : " + str(sum))
-Strings
+
+#Strings
 name = "Tony Stark"
 print(name.upper())
 print(name)
@@ -69,11 +74,13 @@ print(name.find("Stark"))
 print(name.find("stark"))
 print(name.replace("Tony Stark", "Ironman"))
 print(name)
+
 #to check if a character/string is part of the main string
 print("Stark" in name)
 print("S" in name)
 print("s" in name)
-Arithmetic Operators
+
+#Arithmetic Operators
 print(5 + 2)
 print(5 - 2)
 print(5 * 2)
@@ -86,20 +93,21 @@ i = i + 2
 i += 2
 i -= 2
 i *= 2
-Operator Precedence
+
+#Operator Precedence
 result = 3 + 5 * 2 # 16 or 13 ?
 print(result)
-Comments
-# This is a comment & useful for people reading your code
-# This is another line
-Comparison Operators
+
+
+#Comparison Operators
 is_greater = 1 > 5
 is_lesser = 1 < 5
 # 1 <= 5
 # 1 >= 5
 is_not_equal = 1 != 5
 is_equal = 1 == 5
-Logical Operators
+
+#Logical Operators
 # or -> (atleast one is true)
 # and -> (both are true)
 # not -> (reverses any value)
@@ -110,7 +118,8 @@ print(not number > 3)
 print(not number < 3)
 print(number > 3 and number > 1)
 print(number > 3 or number > 1)
-If statements
+
+#If statements
 age = 13
 if age >= 18:
 print("you are an adult")
@@ -120,8 +129,9 @@ print("you are a child")
 else:
 print("you are in school")
 print("thank you")
-Let’s build a Calculator
-#Our Calculator
+
+
+#My Calculator
 first = input("Enter first number : ")
 second = input("Enter second number : ")
 first = int(first)
@@ -140,12 +150,15 @@ elif operator == "%":
 print(first % second)
 else:
 print("Invalid Operation")
-Range in Python
-range() function returns a range object that is a sequence of numbers.
+
+
+#Range in Python
+#range() function returns a range object that is a sequence of numbers.
 numbers = range(5)
 print(numbers)
 For iteration (see For Loop section)
-While Loop
+
+#While Loop
 i = 1
 while(i <= 5):
 print(i)
@@ -158,14 +171,19 @@ i = 5
 while(i >= 1):
 print(i * "*")
 i = i - 1
-For Loop (to iterate over a list)
+
+
+#For Loop (to iterate over a list)
+
 for i in range(5):
 print(i)
 i = i + 1
 for i in range(5):
 print(i * "*")
 i = i + 1
-Lists
+
+
+#Lists
 List is a complex type in Python.
 friends = ["amar", "akbar", "anthony"]
 print(friends[0])
@@ -177,68 +195,87 @@ print(friends)
 print(friends[0:2]) #returns a new list
 for friend in friends:
 print(friend)
-List Methods :
+
+
+#List Methods :
 marks = ["english", 95, "chemistry", 98]
 marks.append("physics")
 marks.append(97)
 print(marks)
+
 marks.insert(0, "math")
 marks.insert(1, 99)
 print(marks)
+
 print("math" in marks)
+
 print(len(marks)/2)
 marks.clear()
 print(marks)
+
 i = 0
 while i < len(marks):
 print(marks[i])
 print(marks[i+1])
 i = i + 2
-Break & Continue
+
+
+#Break & Continue
 students = ["ram", "shyam", "kishan", "radha", "radhika"]
 for student in students:
 if(student == "radha"):
 break
 print(student)
+
 for student in students:
 if(student == "kishan"):
 continue
 print(student)
-Tuples
-They are like lists (sequence of objects) but they are immutable i.e. once they have been
-defined we cannot change them.
-Parenthesis in tuples are optional.
+
+
+#Tuples
+# They are like lists (sequence of objects) but they are immutable i.e. once they have been
+# defined we cannot change them.
+# Parenthesis in tuples are optional.
+
 marks = (95, 98, 97, 97)
 #marks[0] = 98
 print(marks.count(97))
 print(marks.index(97))
-Sets
-Sets are a collection of all unique elements.
-Indexing is not supported in sets.
+
+
+# #Sets
+# Sets are a collection of all unique elements.
+# Indexing is not supported in sets.
+
 marks = {98, 97, 95, 95}
 print(marks)
 for score in marks:
 print(score)
-Dictionary
-Dictionary is an unordered collection of Items. Dictionary stores a (key, value) pair.
+
+
+# Dictionary
+# Dictionary is an unordered collection of Items. Dictionary stores a (key, value) pair.
+
 marks = {"math" : 99, "chemistry" : 98, "physics" : 97}
 print(marks)
 print(marks["chemistry"])
+
 marks["english"] = 95
 print(marks)
+
 marks["math"] = 96
 print(marks)
-Functions in Python
-Function is a piece of code that performs some task. (In a tv remote, each button
-performs a functions, so a function is like that button in code)
+
+
+# Functions in Python
 There are 3 types of functions in Java :
 a. In-built functions
-# int() str() float() min() range() max()
+int() str() float() min() range() max()
+
+
 b. Module functions
-Module is a file that contains some functions & variables which can be imported
-for use in other files.
-Each module should contain some related tasks
-Example : math, random, string
+# Example : math, random, string
 import math
 print(dir(math))
 import random
@@ -247,7 +284,15 @@ import string
 print(dir(string))
 from math import sqrt
 print(sqrt(4))
+
+
 c. User-defined functions
+
 def sum(a, b=4):
 print(a + b)
 sum(1, 2)
+
+
+
+
+
